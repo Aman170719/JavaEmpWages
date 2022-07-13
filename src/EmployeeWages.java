cd5788e47a7ea726a328801728994c7ee7498760
Aman <aman.kumar99499@gmail.com>
2022-07-13 14:48:07 +0530

    Attendance

--- src/EmployeeWages.java
+++ src/EmployeeWages.java
@@ -1,5 +1,13 @@
 public class EmployeeWages {
-    public static void main(String[] args) {
+    public static void main(String[] arg) {
         System.out.println("Welcome To The Emp Wages Calculator Stimulator To Check Salary");
+
+        int isFullTime = 1;
+        double value = Math.floor(Math.random() * 10) % 2;
+        if (value == isFullTime) {
+            System.out.println("Emp is Present");
+        } else
+            System.out.println("EMp is Absent");
+
     }
-}
+        }
