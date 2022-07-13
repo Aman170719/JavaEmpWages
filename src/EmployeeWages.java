34c5fc56881023e26db97741374cdfcb9c954830
Aman <aman.kumar99499@gmail.com>
2022-07-13 15:36:57 +0530

    Switch Case

--- src/EmployeeWages.java
+++ src/EmployeeWages.java
@@ -1,18 +1,42 @@
+import java.util.Scanner;
+
 public class EmployeeWages {
     public static void main(String[] arg) {
         System.out.println("Welcome To The Emp Wages Calculator Stimulator To Check Salary");
 
-        int Full_time=8;
-        int Part_time=4;
-        int Wages_pr_hr =20;
+        int Full_time = 8;
+        int Part_time = 4;
+        int Wages_pr_hr = 20;
         int Present = 1;
         double value = Math.floor(Math.random() * 10) % 2;
-        if (value == Present) {
-            System.out.println("Employee is Present");
-            System.out.println("Daily wages of an Employee is : " +Full_time * Wages_pr_hr);
-            System.out.println("Daily wages of an Employee is : " +Part_time * Wages_pr_hr);
-        } else
-            System.out.println("Employee is Absent");
 
+        if (value == Present) {
+            System.out.println("Employee is Present ");
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Enter the 1 for Full_time And 2 For Part_time");
+            int i = scan.nextInt();
+            scan.close();
+            switch (i) {
+                case 1:
+                    System.out.println("Employee is Present Full Time");
+                    System.out.println("Daily wages of an Employee is : " + Full_time * Wages_pr_hr);
+                    break;
+                case 2:
+                    System.out.println("Employee is Present Part Time");
+                    System.out.println("Daily wages of an Employee is : " + Part_time * Wages_pr_hr);
+                    break;
+                default:
+                    System.out.println("You Have Entered Wrong Number ");
+                       }
+                                 }
+
+                else {
+            System.out.println("Employee is Absent");
         }
-        }
+
+
+    }
+}
+
+
+
