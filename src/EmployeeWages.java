05aba4e83e0175b79ece052a2d98698aabdd1c7e
Aman <106363389+Aman170719@users.noreply.github.com>
2022-07-13 14:51:32 +0530

    Update EmployeeWages.java

--- src/EmployeeWages.java
+++ src/EmployeeWages.java
@@ -1,13 +1,13 @@
 public class EmployeeWages {
     public static void main(String[] arg) {
-        System.out.println("Welcome To The Emp Wages Calculator Stimulator To Check Salary");
+        System.out.println("Welcome To The Employee Wages Calculator Stimulator To Check Salary");
 
         int isFullTime = 1;
         double value = Math.floor(Math.random() * 10) % 2;
         if (value == isFullTime) {
-            System.out.println("Emp is Present");
+            System.out.println("Employee is Present");
         } else
-            System.out.println("EMp is Absent");
+            System.out.println("Employee is Absent");
 
     }
         }
