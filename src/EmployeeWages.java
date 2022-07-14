2f5edfb96e05aff6ce76512f175b41e5dedc8a98
Aman <aman.kumar99499@gmail.com>
2022-07-14 08:11:42 +0530

    Conditional

--- src/EmployeeWages.java
+++ src/EmployeeWages.java
@@ -5,41 +5,18 @@ public class EmployeeWages {
         System.out.println("Welcome To The Emp Wages Calculator Stimulator To Check Salary");
 
         int Full_time = 8;
-        int Part_time = 4;
         int Wages_pr_hr = 20;
-        int Present = 1;
         int Days_in_month = 20;
-        double value = Math.floor(Math.random() * 10) % 2;
+        int Working_hrs= 100;
 
-        if (value == Present) {
-            System.out.println("Employee is Present ");
-            Scanner scan = new Scanner(System.in);
-            System.out.println("Enter the 1 for Full_time And 2 For Part_time");
-            int i = scan.nextInt();
-            scan.close();
-            switch (i) {
-                case 1:
-                    System.out.println("Employee is Present Full Time");
-                    System.out.println("Daily wages of an Employee is : " + Full_time * Wages_pr_hr);
-                    System.out.println("Monthly wages of an Employee is : " + (Full_time * Wages_pr_hr)* Days_in_month);
-                    break;
-                case 2:
-                    System.out.println("Employee is Present Part Time");
-                    System.out.println("Daily wages of an Employee for Part Time is : " + Part_time * Wages_pr_hr);
-                    System.out.println("Monthly wages of an Employee for Part Time is : " + (Part_time * Wages_pr_hr)* Days_in_month);
-                    break;
-                default:
-                    System.out.println("You Have Entered Wrong Number ");
-                       }
-                                 }
-
-                else {
-            System.out.println("Employee is Absent");
+        for (int i = 1; i <= Days_in_month; i++) {
+            int wages = (Wages_pr_hr * Full_time);
+            System.out.println("day : " + i + " Salary is : " + wages);
         }
+        for (int i = 1; i <= Working_hrs; i++) {
+            int wages2 = (Working_hrs * Full_time);
+            System.out.println("day : " + i + " Salary is : " + wages2);
 
-
+        }
     }
 }
-
-
-
