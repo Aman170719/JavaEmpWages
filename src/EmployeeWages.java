3d80be024300f50d111252701c93ed731f2feda2
Aman <106363389+Aman170719@users.noreply.github.com>
2022-08-04 08:18:50 +0530

    Update EmployeeWages.java

--- src/EmployeeWages.java
+++ src/EmployeeWages.java
@@ -1,5 +1,5 @@
 public class EmployeeWages {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         System.out.println("Welcome To The Emp Wages Calculator Stimulator To Check Salary");
     }
 }
